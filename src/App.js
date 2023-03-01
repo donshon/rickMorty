6320b5943b0ecb006e5a30f0a284100bbc48686f
Shawn <shawnche35@gmail.com>
2023-02-28 21:40:16 -0800

    hash

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Homepage from './pages/Homepage/Homepage';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, HashRouter} from 'react-router-dom';
 import About from './pages/About/About';
 import CharacterDetails from './pages/CharacterDetails/CharacterDetails';
 import UserContextProvider from './contexts/UserContext';
